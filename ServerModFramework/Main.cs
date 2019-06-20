16603795f6a6a1ffe80bece85268776d0d41b31e
Administrator <admin@example.com>
2019-06-20 22:10:35 +0800

    开始开发服务器框架功能

---
+++ ServerModFramework/Main.cs
@@ -0,0 +1,20 @@
+﻿using Harmony12;
+using HoldfastGame;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace ServerModFramework
+{
+    public class Framework
+    {
+        public static UnityModManager.ModEntry.ModLogger logger;
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            logger = modEntry.Logger;
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            logger.Log("先生们女士们加载完成");
+            return true;
+        }
+    }
+}
