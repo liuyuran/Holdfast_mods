8682bd9c09d0b04c209d9d0820378bd2ac8f07cc
Administrator <admin@example.com>
2019-06-23 16:03:04 +0800

    add some delegate about server command

---
+++ CompereRobot/Main.cs
@@ -0,0 +1,26 @@
+﻿using ServerModFramework;
+using UnityModManagerNet;
+using Harmony12;
+using System.Reflection;
+
+namespace CompereRobot
+{
+    public class Main
+    {
+        private static UnityModManager.ModEntry.ModLogger logger;
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            logger = modEntry.Logger;
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Framework.countDownDelegate += timelineLoop;
+            logger.Log("虚拟主持人加载完成");
+            return true;
+        }
+
+        private static void timelineLoop(int time)
+        {
+            //
+        }
+    }
+}
--- ServerModFramework/Main.cs
+++ ServerModFramework/Main.cs
@@ -1,6 +1,8 @@
 ﻿using Harmony12;
 using HoldfastGame;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Timers;
 using uLink;
@@ -8,15 +10,19 @@ using UnityModManagerNet;
 
 namespace ServerModFramework
 {
-    public delegate void CountDown();
+    public delegate void CountDown(int roundTime);
     public delegate void PlayerJoin(ulong steamId);
     public delegate void PlayerLeave(ulong steamId);
+    public delegate string AdminMessage(string command);
+    public delegate IConsoleCommand AddCommandList();
 
     public class Framework
     {
-        public static CountDown countDownDelegate = delegate () { };
+        public static CountDown countDownDelegate = delegate (int roundTime) { };
         public static PlayerJoin playerJoinDelegate = delegate (ulong steamId) { };
         public static PlayerLeave playerLeaveDelegate = delegate (ulong steamId) { };
+        public static AdminMessage adminMessageDelegate = delegate (string message) { return ""; };
+        public static AddCommandList addCommandListDelegate = delegate () { return null; };
 
         private static UnityModManager.ModEntry.ModLogger logger;
         private static Hashtable steamIdToLocalId = new Hashtable();
@@ -39,12 +45,12 @@ namespace ServerModFramework
             timer.Start();
             timer.Elapsed += new System.Timers.ElapsedEventHandler(delegate (object source, ElapsedEventArgs e)
             {
-                countDownDelegate();
+                countDownDelegate(getRoundTime());
             });
         }
 
         [HarmonyPatch(typeof(ServerPlayerActionsLogFileHandler), "AddPlayerJoinedEntry")]
-        private static class Join_Patch
+        private static class PlayerJoin_Patch
         {
             static void Postfix(int playerID)
             {
@@ -66,7 +72,7 @@ namespace ServerModFramework
         }
 
         [HarmonyPatch(typeof(ServerRoundPlayerManager), "RemovePlayer")]
-        private static class Remove_Patch
+        private static class PlayerRemove_Patch
         {
             static void Postfix(NetworkPlayer networkPlayer)
             {
@@ -75,14 +81,61 @@ namespace ServerModFramework
             }
         }
 
+        [HarmonyPatch(typeof(ServerChatHandler), "HandleAsAdminCommand")]
+        private static class ChatAdminCommand_Patch
+        {
+            static void Postfix(string entryText, RoundPlayer player, out bool __result)
+            {
+                string text;
+                bool flag2;
+                Exception ex;
+                string input = null;
+                foreach (AdminMessage processor in adminMessageDelegate.GetInvocationList())
+                {
+                    input = processor(entryText);
+                    if (input != null)
+                    {
+                        ServerComponentReferenceManager.ServerInstance.console
+                            .ExecuteInput(input, player.NetworkPlayerID, out text, out flag2, out ex, true);
+                        break;
+                    }
+                }
+                __result = input != null;
+            }
+        }
+
+        [HarmonyPatch(typeof(GameConsolePanel), "SetCommands")]
+        private static class ServerCommands_Patch
+        {
+            static void Prefix(ref IEnumerable<IConsoleCommand> commands)
+            {
+                List<IConsoleCommand> list = new List<IConsoleCommand>(commands);
+                IConsoleCommand command = null;
+                foreach (AddCommandList processor in addCommandListDelegate.GetInvocationList())
+                {
+                    command = processor();
+                    if (command != null)
+                    {
+                        commands.Add(command);
+                    }
+                }
+                commands = list;
+            }
+        }
+
         public static void sendMessage(int steamId, string message)
         {
             ServerComponentReferenceManager.ServerInstance.serverAdminBroadcastMessageManager.PrivateMessage(-1, steamId, message);
         }
 
-        public static int getRoundTime()
+        public static void sendMessage(string message)
         {
-            return (int) ServerComponentReferenceManager.ServerInstance.serverRoundTimerManager.secondsToRoundEnd;
+            ServerComponentReferenceManager.ServerInstance.serverAdminBroadcastMessageManager.BroadcastAdminMessage(message);
+        }
+
+        private static int getRoundTime()
+        {
+            return (int) ServerComponentReferenceManager.ServerInstance.serverRoundTimerManager.secondsSinceRoundStarted;
         }
     }
 }
