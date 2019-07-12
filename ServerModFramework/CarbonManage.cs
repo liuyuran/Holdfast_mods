d0688d557d33e90be76d01f3f40efd983237ee05
刘宇然 <liuyuran719829142@hotmail.com>
2019-07-12 08:16:24 +0800

    update0711

--- ServerModFramework/CarbonManage.cs
+++ ServerModFramework/CarbonManage.cs
@@ -9,53 +9,86 @@
 
 using Harmony12;
 using HoldfastGame;
+using System.Collections.Generic;
+using uLink;
+using UnityEngine;
 
 namespace ServerModFramework
 {
     public static partial class Framework
     {
-        [HarmonyPatch(typeof(ServerCarbonPlayersManager), "AddCarbonPlayers")]
-        private static class CarbonAddCarbonPlayer_Patch
+        private static List<int> carbonList = new List<int>();
+        private static Dictionary<int, CarbonPlayer> carbonLink = new Dictionary<int, CarbonPlayer>();
+        private static ServerComponentReferenceManager instant => ServerComponentReferenceManager.ServerInstance;
+
+        public class CarbonPlayer
         {
-            static bool Prefix(int total, float interval, ref bool __result)
+            private NetworkPlayer networkPlayer;
+            public NetworkPlayer getNetworkPlayer => networkPlayer;
+            public CarbonPlayer(NetworkPlayer networkPlayer)
             {
-                __result = true;
-                return false;
+                this.networkPlayer = networkPlayer;
+            }
+            public void carbonCommandSwitchWeapon(WeaponType weaponType)
+            {
+                ServerRoundPlayer serverRoundPlayer = instant.serverRoundPlayerManager.ResolveServerRoundPlayer(networkPlayer.id);
+                if (serverRoundPlayer != null)
+                {
+                    Weapon weapon = serverRoundPlayer.WeaponHolder.AvailableWeapons.GetWeapon(weaponType, serverRoundPlayer.SpawnData.Faction);
+                    if (!(weapon == null))
+                    {
+                        instant.serverWeaponHolderManager.BroadcastSwitchWeapon(serverRoundPlayer.NetworkPlayerID, weapon.identifier.ID);
+                    }
+                }
             }
         }
 
-        [HarmonyPatch(typeof(ServerCarbonPlayersManager), "RemoveCarbonPlayer")]
-        private static class CarbonRemoveCarbonPlayer_Patch
+        public static int addCarbonPlayer()
         {
-            static bool Prefix(int playerID)
+            NetworkPlayer networkPlayer = Network.NetworkClientAllocator.Allocate();
+            networkPlayer.isCarbonPlayer = true;
+            ClientSteamAuthSettings auth = new ClientSteamAuthSettings
             {
-                return false;
-            }
+                SteamID = 0UL,
+                SteamCBAuthTicket = 0,
+                SteamPAuthTicket = new byte[0],
+                MachineID = SystemInfo.deviceUniqueIdentifier,
+                MachineSecurityID = StringCipher.Hash(SystemInfo.deviceUniqueIdentifier, 0UL)
+            };
+            instant.serverGameManager.HandleSteamAuthSettings(auth, networkPlayer);
+            carbonList.Add(networkPlayer.id);
+            carbonLink.Add(networkPlayer.id, new CarbonPlayer(networkPlayer));
+            return networkPlayer.id;
         }
 
-        [HarmonyPatch(typeof(ServerCarbonPlayersManager), "Update")]
-        private static class CarbonUpdate_Patch
+        public static void removeCarbonPlayer(int id)
         {
-            static bool Prefix()
-            {
-                return false;
-            }
+            if (!carbonList.Contains(id) || !carbonLink.ContainsKey(id)) return;
+            Network.NetworkClientAllocator.Deallocate(carbonLink[id].getNetworkPlayer, 0.0);
+            carbonList.Remove(id);
+            carbonLink.Remove(id);
         }
 
-        [HarmonyPatch(typeof(ServerCarbonPlayersManager), "FixedUpdate")]
-        private static class CarbonFixedUpdate_Patch
+        [HarmonyPatch(typeof(ServerCarbonPlayersManager), "RequestingInitialDetailsRPC")]
+        private static class CarbonManager_RequestingInitialDetailsRPC_Patch
         {
-            static bool Prefix()
+            static bool Prefix(NetworkPlayer networkPlayer, GameServerInitialDetails packet)
             {
-                return false;
-            }
-        }
-
-        [HarmonyPatch(typeof(ServerCarbonPlayersManager), "InitializeOnMap")]
-        private static class CarbonInitializeOnMap_Patch
-        {
-            static bool Prefix()
-            {
+                int characterFaceIdentifier = 0;
+                PlayerInitialDetails playerInitialDetails = new PlayerInitialDetails
+                {
+                    Name = HomelessMethods.GenerateRandomWord(10),
+                    CharacterVoicePitch = 1f,
+                    CharacterFaceIdentifier = characterFaceIdentifier
+                };
+                instant.serverGameManager.HandlePlayerInitialDetails(playerInitialDetails, networkPlayer);
+                AutonomousPlayerInputHandler input = new AutonomousPlayerInputHandler(instant.commonGlobalVariables.BotAutoMove);
+                CarbonPlayerRepresentation value = new CarbonPlayerRepresentation
+                {
+                    playerID = networkPlayer.id,
+                    input = input,
+                    networkPlayer = networkPlayer
+                };
                 return false;
             }
         }
