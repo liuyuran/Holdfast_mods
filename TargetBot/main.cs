90175155145769416e3b9a23cf8fc416426008e3
liuyuran <liuyuran719829142@hotmail.com>
2019-08-25 22:48:22 +0800

    暂存

--- TargetBot/main.cs
+++ TargetBot/main.cs
@@ -1,4 +1,5 @@
 ﻿using Harmony12;
+using ServerModFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,28 @@ namespace TargetBot
             modEntry.OnToggle = OnToggle;
             logger = modEntry.Logger;
             logger.Log("插件开始加载 patch id:" + modEntry.Info.Id);
+            Framework.playerCommandDelegate += Framework_playerCommandDelegate;
             var harmony = HarmonyInstance.Create(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             logger.Log("插件注入完毕");
             return true;
+        }
+
+        private static string Framework_playerCommandDelegate(string modName, object[] arguments, ulong steamID, out bool success)
+        {
+            if (modName != "targetBot")
+            {
+                success = false;
+                return null;
+            }
+            string command = (string)arguments[0];
+            switch (command)
+            {
+                case "add":
+                default:
+                    success = false;
+                    return string.Format("不合法的操作符: %s", command);
+            }
         }
 
         public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
