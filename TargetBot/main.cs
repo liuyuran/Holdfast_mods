9d2156c41f6e24ecfbed50ebc4d88e9bf006a1a1
liuyuran <liuyuran719829142@hotmail.com>
2019-08-25 22:09:50 +0800

    初始化功能

---
+++ TargetBot/main.cs
@@ -0,0 +1,33 @@
+﻿using Harmony12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityModManagerNet;
+
+namespace TargetBot
+{
+    public class Main
+    {
+        public static bool enabled;
+        public static UnityModManager.ModEntry.ModLogger logger;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+            logger = modEntry.Logger;
+            logger.Log("插件开始加载 patch id:" + modEntry.Info.Id);
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            logger.Log("插件注入完毕");
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+    }
+}
